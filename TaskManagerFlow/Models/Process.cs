3b70720ff0ff8e3cdd007f4f76bac79f5f919bb7
Denys Baranov <265denedge@gmail.com>
2019-04-06 04:14:53 +0300

    Добавьте файлы проекта.

---
+++ TaskManagerFlow/Models/Process.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerFlow.Models
+{
+    class Process
+    {
+        public String Name { get; set; }
+        public String ProcessId { get; set; }
+        public bool Active { get; set; }
+        public String ProcessGpu { get; set; }
+        public String ProcessRam { get; set; }
+        public String Threads { get; set; }
+        public String UserName { get; set; }
+        public String Path { get; set; }
+        public String FileName { get; set; }
+        public DateTime RunDate { get; set; }
+
+        Process(DateTime runDate, string fileName, string path, string userName, string threads, bool active, string processId, string name)
+        {
+            RunDate = runDate;
+            FileName = fileName;
+            Path = path;
+            UserName = userName;
+            Threads = threads;
+            Active = active;
+            ProcessId = processId;
+            Name = name;
+        }
+
+    }
+}
---
+++ TaskManagerFlow/ViewModels/TaskManagerViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerFlow.ViewModels
+{
+    class TaskManagerViewModel
+    {
+        private Process[] _processList;
+        public Process[] ProcessList
+        {
+            get
+            {
+                return _processList;
+
+            }
+            set
+            {
+                _processList = value;
+
+            }
+        }
+
+        public TaskManagerViewModel()
+        {
+            ProcessList = GenerateProcessList();
+        }
+        private Process[] GenerateProcessList()
+        {
+            return Process.GetProcesses();
+        }
+    }
+}
